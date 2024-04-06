5987b28e4b3ea92d3a6b6be435cf9fedfc0fd876
ShubhamPatel12499 <shubhampatel12499@gmail.com>
2024-04-07 00:35:42 +0530

    Backend of Task Management App

---
+++ Task-Management-Backend/index.js
@@ -0,0 +1,29 @@
+const express=require("express")
+const {connection}=require("./config/db")
+const {taskRouter}=require("./routes/Task.route")
+
+const cors = require('cors')
+
+const app=express()
+require("dotenv").config();
+
+app.use(cors({
+    origin:"*"
+}))
+app.use(express.json())
+
+
+app.use("/tasks",taskRouter)
+
+
+app.listen(process.env.port,async()=>{
+    try{
+        await connection
+        console.log("Connected to DB")
+    }
+    catch(err){
+      console.log("Not connected to DB")
+      console.log(err)
+    }
+    console.log(`Server is running on ${process.env.port}`)
+})
